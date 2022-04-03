df72f9fb16734e20c86a84af20ece33d53bc8d88
Vahtee Boo <vahtee.boo@gmail.com>
2022-04-03 12:32:50 +0300

    Initial Commit

---
+++ TimerSTBC/TimerSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerSTBCApp.swift
+//  TimerSTBC
+//
+//  Created by Vahtee Boo on 03.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TimerSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
